f5c034099dce717f9ecabcaf87c6d684e1aaeca1
VaibhavSimusolar <vvaidya@simusolar.com>
2021-10-09 17:55:33 +0300

    Combined 'Dashboard' into Devices Page

--- Airlink/Airlink/Views/DevicePage.xaml.cs
+++ Airlink/Airlink/Views/DevicePage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Airlink.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,9 +13,28 @@ namespace Airlink.Views
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class DevicePage : ContentPage
     {
+        ServersViewModel _viewModel;
         public DevicePage()
         {
             InitializeComponent();
+            BindingContext = _viewModel = new ServersViewModel();
+        }
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            _viewModel.OnAppearing();
+        }
+        private void ListView_OnItemSelected(Object sender, SelectedItemChangedEventArgs e)
+        {
+            if (e.SelectedItem != null)
+            {
+                //((BlePeripheralViewModel)e.SelectedItem).IsExpanded = !((BlePeripheralViewModel)e.SelectedItem).IsExpanded;
+                ((ListView)sender).SelectedItem = null;
+            }
+        }
+
+        private void ListView_OnItemTapped(Object sender, ItemTappedEventArgs e)
+        {
         }
     }
 }
